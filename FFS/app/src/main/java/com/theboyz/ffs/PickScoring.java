1fca9796327e2e12a9c2652f057e6b21d8128e08
Chase Uphaus <chase.uphaus@yahoo.com>
2019-11-27 02:27:16 -0600

    Added PickScoring page

---
+++ FFS/app/src/main/java/com/theboyz/ffs/PickScoring.java
@@ -0,0 +1,13 @@
+package com.theboyz.ffs;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PickScoring extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pick_scoring);
+    }
+}
